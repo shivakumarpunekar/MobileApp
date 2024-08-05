96bf27fe4d029b844d8589a78416edb3b9edd977
graghory john <graghoryjohn78@gmail.com>
2024-08-05 18:23:36 +0530

    implemented in switch page

--- src/Admin/Switch.js
+++ src/Admin/Switch.js
@@ -1,14 +1,51 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Switch, StyleSheet } from "react-native";
 
-const Switch = () => {
-    return(
-        <View>
-            <Text>
-                This is a Switch page.
-            </Text>
+const SwitchPage = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>This is a Switch page.</Text>
+            <View style={styles.switchContainer}>
+                <Text style={styles.switchText}>{isEnabled ? "On" : "Off"}</Text>
+                <Switch
+                    trackColor={{ false: "#767577", true: "#767557" }}
+                    thumbColor={isEnabled ? "#000" : "#f4f3f4"}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                    style={styles.switch}
+                />
+            </View>
         </View>
     );
 };
 
-export default Switch;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F6F3E7',
+    },
+    text: {
+        fontSize: 18,
+        marginBottom: 20,
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    switchText: {
+        fontSize: 18,
+        marginRight: 10,
+    },
+    switch: {
+        transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
+    },
+});
+
+export default SwitchPage;
